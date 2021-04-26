a653ce4182da4172ed47b5a429b8e082618f5646
nojiyoon <neutral.sth@gmail.com>
2021-04-26 14:25:00 +0900

    [ADD]

---
+++ README.md
@@ -0,0 +1,30 @@
+# korbert_qa_demo
+ETRI에서 제공하는 korbert pretrained 모델을 이용해 qa_demo 를 구현합니다.
+
+## 코드 참조
+henryzxu의 bert-qa-demo를 참고하여 작성하였습니다.
+
+## 사용 방법
+1. git repository를 clone 합니다.  
+    - `git clone https://github.com/JeightAn/korbert_qa_demo.git`
+
+2. git branch를 변경합니다.
+    - `git switch test`
+    
+3. 가상환경을 설정합니다.
+    - `conda create -n qa_demo python=3.6`
+
+4. 가상환경을 실행합니다.
+    - `conda activate qa_demo`
+
+5. 필요한 모듈을 설치합니다.
+    - `pip install -r requirements.txt`
+
+6. drive에 업로드 된 pretrained_model(finetuning)을 다운로드 받습니다.
+    - app 폴더 안에 pretrained_korbert 디렉토리를 저장해주세요.
+
+7. 플라스크 앱 경로를 설정합니다.
+    - `export FLASK_APP=app/server_local.py`
+
+8. 실행합니다.
+    - `flask run`
---
+++ app/static/script.js
@@ -0,0 +1,110 @@
+$(function() {
+    $('button#process').on('click', function() {
+      $.getJSON($SCRIPT_ROOT + '/_input_helper', {
+        question_data: $('#question-data').val()
+      }, function(data) {
+        $("#result").prepend(data.result);
+        $("#question-data").val("");
+        var hilite = new Function(data.highlight_script);
+        hilite()
+      });
+      return false;
+    });
+  });
+
+$('textarea').each(function () {
+  this.setAttribute('style', 'height:' + (this.scrollHeight) + 'px;overflow-y:hidden;');
+}).on('input', function () {
+  this.style.height = 'auto';
+  this.style.height = (this.scrollHeight) + 'px';
+});
+
+$(function() {
+    $('button#store-btn').on('click', function() {
+      $.getJSON($SCRIPT_ROOT + '/_store_context', {
+        text_data: $('#text-data').val(),
+      }, function(data) {
+          if (data.context) {
+              $("#store-context").hide();
+              $("#text-data").val("");
+              $("#reset-context").show();
+              $("#question-input").show();
+              $("#question-data").val("");
+              $('textarea').trigger('input');
+              $("#context-title").html(data.title);
+              $("#context-data").html(data.context);
+              if (data.clear_files) {
+                  dz.removeAllFiles();
+              }
+          }
+      });
+      return false;
+    });
+  });
+
+$('button#reset-btn').on('click', function() {
+    $("#store-context").show();
+    $("#reset-context").hide();
+    $("#question-input").hide();
+
+    $(".history").show();
+
+    var hist = $("#history");
+    hist.prepend("<br>");
+    hist.prepend($("#result").html());
+    hist.prepend($("#context-data").html().replace('<span class="hilite">', "").replace('</span>', ""));
+    hist.prepend(("<h4>" + $("#context-title").html() + "</h4>").replace('<span class="hilite">', "").replace('</span>', ""));
+
+    $("#context-title").html("");
+    $("#context-data").html("");
+    $("#result").html("");
+    $('textarea').trigger('input');
+});
+
+$('a#toggle-history').on('click', function() {
+    var hist_toggle = $('#toggle-history');
+    hist_toggle.text() === "(hide)" ? hist_toggle.text("(show)") : hist_toggle.text("(hide)");
+    $("#history").toggle();
+});
+
+$(function() {
+    $('button#wiki-btn').on('click', function() {
+      $.getJSON($SCRIPT_ROOT + '/_wiki_api', {
+        my_input: $('#myInput').val(),
+      }, function(data) {
+        $("#text-data").val(data.context);
+        $('textarea').trigger('input');
+      });
+      return false;
+    });
+});
+
+/*
+$(function() {
+    $('button#random-btn').on('click', function() {
+      $.getJSON($SCRIPT_ROOT + '/_random_page', {
+      }, function(data) {
+        $("#text-data").val(data.context);
+        $('textarea').trigger('input');
+      });
+      return false;
+    });
+});
+*/
+
+function highlight(element, start, end) {
+    if (start > -1) {
+        var item = $(element);
+        var str = item.data("origHTML");
+        if (!str) {
+            str = item.html();
+            item.data("origHTML", str);
+        }
+        str = str.substr(0, start) +
+            '<span class="hilite">' +
+            str.substr(start, end - start + 1) +
+            '</span>' +
+            str.substr(end + 1);
+        item.html(str);
+    }
+}
